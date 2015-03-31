12c28e2826a71b211cec2d403d2cfac30fa56ed7
ophyneus <ophyneus@tlu.ee>
2015-03-31 11:34:22 +0300

    proovifail

--- Kolmkola.java
+++ Kolmkola.java
@@ -1,6 +1,10 @@
-public class Kolmkola{  
-	public int pohitoon;
-	public Kolmkola(pohitoon){
-		this.pohitoon = pohitoon;
+public class Kolmkola{
+	public int _pohitoon;
+	public int Kolmkola(int pohitoon){
+		_pohitoon = pohitoon;
+		return pohitoon;
 	}
+	public int pohitoon(){return _pohitoon;}
+	public int terts(){return _pohitoon+4;}
+	public int kvint(){return _pohitoon+7;}
 }
---
+++ Proov1.java
@@ -0,0 +1,6 @@
+public class Proov1{
+	public static void main(String[] arg){
+		Kolmkola k = new Kolmkola(60);
+		System.out.println(k.pohitoon()+", "+k.terts()+", "+k.kvint());
+	}
+}
