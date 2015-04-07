83accc622864671a5f56100b34edac0610da68ad
ophyneus <ophyneus@tlu.ee>
2015-04-07 11:06:42 +0300

    töötav alamklassidega

--- Kolmkola.java
+++ Kolmkola.java
@@ -1,8 +1,7 @@
 public class Kolmkola{
 	public int _pohitoon;
-	public int Kolmkola(int pohitoon){
+	public Kolmkola(int pohitoon){
 		_pohitoon = pohitoon;
-		return pohitoon;
 	}
 	public int pohitoon(){return _pohitoon;}
 	public int terts(){return _pohitoon+4;}
--- Proov1.java
+++ Proov1.java
@@ -1,6 +1,8 @@
 public class Proov1{
 	public static void main(String[] arg){
 		Kolmkola k = new Kolmkola(60);
+		Kolmkola c = new CKolmkola();
 		System.out.println(k.pohitoon()+", "+k.terts()+", "+k.kvint());
+		System.out.println(c.pohitoon()+", "+c.terts()+", "+c.kvint());
 	}
 }
